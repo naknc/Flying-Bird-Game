6a782ac00e38e10a5ce8b51a8d6dc9fbfc0419ad
naknc <nihan.akinci.35@gmail.com>
2020-04-09 03:20:44 +0300

    ilk işler

---
+++ android/src/com/n/ucankus/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.n.ucankus;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.n.ucankus.UcanKus;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UcanKus(), config);
+	}
+}
