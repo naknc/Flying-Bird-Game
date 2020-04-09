6560686d84e17de9f222b13705a7fe9a9785698a
naknc <nihan.akinci.35@gmail.com>
2020-04-09 04:21:01 +0300

    Background

--- core/src/com/n/ucankus/UcanKus.java
+++ core/src/com/n/ucankus/UcanKus.java
@@ -13,21 +13,19 @@ public class UcanKus extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("back.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
